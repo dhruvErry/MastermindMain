905954107b19cbcf0fec7b4981a23dd80af37d3b
Dhruv Erry <dhruv.erryde@gmail.com>
2020-10-14 20:44:29 +0530

    Test!

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ var engines = require("consolidate");
 var BP = require("body-parser");
 var express = require("express");
 var app = express();
-var room, count = 0;
-var roomz=[]
-var idRoom=[]
+var room,
+  count = 0;
+var roomz = [];
+var idRoom = [];
 
 app.use(BP.urlencoded({ extended: true }));
 
@@ -27,40 +28,37 @@ io.on("connection", (socket) => {
   console.log("Cunectid");
   socket.on("createRoom", (rom) => {
     room = rom;
-    if(!(roomz.hasOwnProperty(room))){
-        roomz[room]=1;
-        socket.join(room);
-        idRoom[socket.id]=room;
-        socket.emit('goUhed', 'g')
-    } 
-    else if(roomz[room]<2){
-        socket.join(room);
-        idRoom[socket.id]=room;
-        socket.emit('goUhed', 'g')
-        roomz[room]=roomz[room]+1;
-    }
-    else
-        socket.emit('eruu', 'e')
-    console.log(roomz)
+    if (!roomz.hasOwnProperty(room)) {
+      roomz[room] = 1;
+      socket.join(room);
+      idRoom[socket.id] = room;
+      socket.emit("goUhed", "g");
+    } else if (roomz[room] < 2) {
+      socket.join(room);
+      idRoom[socket.id] = room;
+      socket.emit("goUhed", "g");
+      roomz[room] = roomz[room] + 1;
+    } else socket.emit("eruu", "e");
+    console.log(roomz);
   });
   socket.on("newPlayer", (naym) => {
     // console.log(naym + " joind " + room);
   });
-  socket.on("ges", data=> {
+  socket.on("ges", (data) => {
     // io.in(room).emit('ges', ges)
     socket.broadcast.to(data.rom).emit("ges", data.guess);
   });
-  socket.on("sub", data=>{
+  socket.on("sub", (data) => {
     socket.broadcast.to(data.rom).emit("sub", data.gesser);
-  })
-  socket.on('curect', q=>{
-    socket.broadcast.to(q).emit('curect', 'j')
-  })
-  socket.on('code', s=>{
-      socket.broadcast.to(s).emit('code', 'g')
-  })
-  socket.on('disconnect', p=>{
-      roomz[idRoom[socket.id]]=roomz[idRoom[socket.id]]-1
-      console.log(roomz)
-  })
+  });
+  socket.on("curect", (q) => {
+    socket.broadcast.to(q).emit("curect", "j");
+  });
+  socket.on("code", (s) => {
+    socket.broadcast.to(s).emit("code", "g");
+  });
+  socket.on("disconnect", (p) => {
+    roomz[idRoom[socket.id]] = roomz[idRoom[socket.id]] - 1;
+    console.log(roomz);
+  });
 });
